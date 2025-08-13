2886cdfcd5a213a560a5a8fb6f0833626b013850
After3ffects <121523105+After3ffects@users.noreply.github.com>
2025-08-13 19:03:39 -0400

    updated readme and finished autoquote

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 For NVIM v0.11.3
+Windows 10/11
+
+Ideas:
+- Toggle color from a list of colorschemes
+- Integrate user capabilities from kickstart and neovim distros
+- Add deleting of autobracket when deleting the first character
--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -13,20 +13,35 @@ local keyskip = function(char)
     if line:sub(col, col) == char then
         return '<Right>'
     else
-        print(line:sub(col, col))
         return char
+    end
+end
+
+local autoquote = function(char)
+    local col = vim.fn.col('.')
+    local line = vim.fn.getline('.')
+    if line:sub(col-1, col-1) ~= char then
+        if line:sub(col, col) == char then
+            return '<Right>'
+        else
+            return char .. char .. '<Left>'
+        end
+    else
+        return char .. char .. '<Left>'
     end
 end
 
 vim.keymap.set('i', '{', '{}<Left>')
 vim.keymap.set('i', '(', '()<Left>')
 vim.keymap.set('i', '[', '[]<Left>')
-vim.keymap.set('i', '\'', '\'\'<Left>', { expr = true})
-vim.keymap.set('i', '"', '\"\"<Left>')
+
+
+
+--vim.keymap.set('i', '\'', '\'\'<Left>')
+--vim.keymap.set('i', '\"', '\"\"<Left>')
 
 vim.keymap.set('i', '}', function() return keyskip('}') end, { expr = true, noremap = true})
 vim.keymap.set('i', ')', function() return keyskip(')') end, { expr = true, noremap = true})
 vim.keymap.set('i', ']', function() return keyskip(']') end, { expr = true, noremap = true})
-vim.keymap.set('i', '\'', function() return keyskip('\'') end, { expr = true, noremap = true})
-vim.keymap.set('i', '"', function() return keyskip('"') end, { expr = true, noremap = true})
-
+vim.keymap.set('i', '\'', function() return autoquote('\'') end, { expr = true, noremap = true})
+vim.keymap.set('i', '"', function() return autoquote('"') end, { expr = true, noremap = true})
