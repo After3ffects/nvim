14d7a5e9671eb03d20a07cff636f7d34033c489b
After3ffects <jacksonkrodgers@gmail.com>
2025-12-25 15:22:12 -0500

    removed underlining for min severity in pylsp

--- lua/plugins/options.lua
+++ lua/plugins/options.lua
@@ -1,3 +1,7 @@
 vim.cmd("colorscheme tokyonight-night")
 builtin = require('telescope.builtin')
 --require("bufferline").setup{}
+
+vim.diagnostic.config({
+  underline = { severity = { min = vim.diagnostic.severity.ERROR } },
+})
