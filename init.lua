c2933b3da5bb721a5654511d043ddb8689717bf4
jkr0dgers <121523105+After3ffects@users.noreply.github.com>
2026-06-02 09:16:05 -0400

    updated init.lua to activate treesitter

--- init.lua
+++ init.lua
@@ -3,3 +3,8 @@ require("keymaps")
 require("plugins.lazy")
 require("plugins.options")
 require("plugins.keymaps")
+vim.api.nvim_create_autocmd('FileType', {
+  callback = function()
+    pcall(vim.treesitter.start)
+  end,
+})
